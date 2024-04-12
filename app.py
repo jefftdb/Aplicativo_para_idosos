b5193e082fd1890ca84906b6ab8e36f2f1dddc12
Jefferson <40587602+jefftdb@users.noreply.github.com>
2024-04-12 13:03:29 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, template_folder=os.path.abspath('view/templates'), static_folder=os.path.abspath("view/static"))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/idosos'
+app.config['SECRET_KEY'] = 'secret'
+
+
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
+
+from controller.login_controller import *
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ controller/login_controller.py
@@ -0,0 +1,56 @@
+from app import app,db
+from flask import render_template,request,redirect,url_for
+from model.login_model import User
+from flask_login import login_user,logout_user
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/register', methods = ['GET' , 'POST'])
+def register():
+    if request.method == 'POST':   
+        name = request.form['name']
+        email = request.form['email']
+        pwd = request.form['password']
+
+        print(name,email, pwd)
+        print('Entrou aqui')
+
+        if name and email and pwd:
+            user = User(name,email,pwd)
+            db.session.add(user)
+            db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods = ['GET' , 'POST'])
+def login():
+
+    if request.method == 'POST':   
+
+        email = request.form['email']
+        pwd = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not user.verify_password(pwd):
+            return redirect(url_for('login'))
+        
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
